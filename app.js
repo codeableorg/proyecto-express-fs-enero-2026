7dac585bb7da94ead72ff9eee5dd3dca437f717c
crehds <carlos15erh@gmail.com>
2026-05-20 19:23:48 -0500

    Refactor: move route handlers to a separate router module

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 import express from "express";
-import { getHealth, getTime } from "./handlers/apiHandlers.js";
-import { getContact, getHome, postContact } from "./handlers/viewHandlers.js";
+
 import { globalErrorHandler } from "./handlers/errorHandler.js";
+import router from "./router.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-// API
-app.get("/api/health", getHealth);
-app.get("/api/time", getTime);
 
-// HTML
-app.get("/", getHome);
-app.get("/contact", getContact);
-app.post("/contact", postContact);
+app.use(router);
 
 app.use(globalErrorHandler);
 
---
+++ router.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { getHealth, getTime } from "./handlers/apiHandlers.js";
+import { getContact, getHome, postContact } from "./handlers/viewHandlers.js";
+const router = Router();
+// API
+router.get("/api/health", getHealth);
+router.get("/api/time", getTime);
+
+// HTML
+router.get("/", getHome);
+router.get("/contact", getContact);
+router.post("/contact", postContact);
+
+export default router;
